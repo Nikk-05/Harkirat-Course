1184bd913e4b26c3e929b00b6b04f2240977e07b
Nikk-05 <singh.nikhil.0528@gmail.com>
2025-06-20 23:30:43 +0530

    tailwind

--- Tailwind-Week-8/src/App.jsx
+++ Tailwind-Week-8/src/App.jsx
@@ -2,32 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RevenueCard from './components/RevenueCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid grid-cols-4'>
+      <RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={13} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
