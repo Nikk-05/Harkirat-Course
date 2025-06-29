31b58e37709ee2fe8c2501c8203aac1fb698276b
Nikk-05 <singh.nikhil.0528@gmail.com>
2025-06-29 21:49:21 +0530

    Remove connection string

--- Postgres Database/src/utils.ts
+++ Postgres Database/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from "pg";
 
 export async function getClient(){
-    const client = new Client("postgresql://neondb_owner:npg_2lgauk3PVWZw@ep-polished-shape-a4vekn9b-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require")
+    const client = new Client("Postgress Connection String");
     await client.connect();
     return client;
 }
